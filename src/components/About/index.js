77728be66631491b6ed7a1bcced5f1906a7eaa08
Sandeep Vittala <applesandy33@gmail.com>
2022-03-26 16:53:10 -0400

    done with About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
-  return (
-    <section className= 'my-5'>
-      <h1 id="about">this is Sandeep</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-    </section>
+  return (  
+      <div class="container px-6">
+        <div class="row gx-5">
+            <div class="col">
+                <div class="p-3 border bg-light">
+                    <h1 id="about">Welcome to Sandeep Vittla's Webpage</h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 }
 
